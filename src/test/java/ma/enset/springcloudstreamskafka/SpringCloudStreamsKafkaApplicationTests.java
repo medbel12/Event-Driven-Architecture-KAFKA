4acadec8e6bc81e7bd1e1353e1ad4b427bad16e8
med <mohamedbel357@gmail.com>
2024-10-27 17:34:38 +0100

    Kafka

---
+++ src/test/java/ma/enset/springcloudstreamskafka/SpringCloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.springcloudstreamskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
